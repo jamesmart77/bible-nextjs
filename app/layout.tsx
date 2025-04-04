f7692850e16f8c17bb27984d529e742c0ac13c0f
jamesmart77 <jamesmart77@gmail.com>
2025-04-04 06:23:01 -0400

    auth0 added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from '@vercel/analytics/next';
+import { auth0 } from "@/lib/auth0"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -18,13 +19,31 @@ export const metadata: Metadata = {
   description: "Delight in God's word without the distractions",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth0.getSession();
+
+
   return (
     <html lang="en">
+      <header>
+        {!session && (
+          <>
+            <a href="/auth/login?screen_hint=signup">Sign up</a>
+            {' '}
+            <a href="/auth/login">Log in</a>
+          </>
+        )}
+        {session && (
+          <>
+          <span>Welcome, {session.user.name}!</span>
+          <a href="/auth/logout">Log out</a>
+          </>
+        )}
+      </header>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
