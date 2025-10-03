1601be8b6e99e1e644a4b187c31287df3364f6a1
jamesmart77 <jamesmart77@gmail.com>
2025-10-03 18:18:57 -0400

    logging

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -4,7 +4,9 @@ export async function GET(request: Request) {
   if (
     request.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
   ) {
+    console.error("Unauthorized - be gone!");
     return new NextResponse("Unauthorized - be gone!", { status: 401 });
   }
+  console.log("Cron job executed");
   return NextResponse.json({ ok: true });
 }
