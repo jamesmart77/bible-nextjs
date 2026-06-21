bec129e53f53761eeb447e1ac6f3d884bc8d4f41
James Martineau <jamesmartineau@Jamess-MacBook-Air.local>
2026-06-20 23:29:51 -0400

    fix: share button placement

--- app/components/passages/ShareSelectedVersesButton.tsx
+++ app/components/passages/ShareSelectedVersesButton.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { IconButton } from "@chakra-ui/react";
+import { Box, IconButton } from "@chakra-ui/react";
 import { Bounce } from "react-awesome-reveal";
 import { RiShareFill } from "react-icons/ri";
+import useDeviceBreakpoint from "@/app/hooks/useDeviceBreakpoint";
 
 type Props = {
   isVisible: boolean;
@@ -13,20 +14,28 @@ export default function ShareSelectedVersesButton({
   isVisible,
   onClick,
 }: Props) {
+  const { isMobile } = useDeviceBreakpoint();
+
   if (!isVisible) return null;
 
+  console.log({ isMobile });
+
   return (
-    <Bounce
-      triggerOnce
-      style={{ position: "fixed", bottom: "1.5rem", right: "1rem" }}
+    <Box
+      position="fixed"
+      bottom={isMobile ? "6rem" : "1.5rem"}
+      right="1rem"
+      zIndex={2}
     >
-      <IconButton
-        aria-label="Share selected verses"
-        rounded="full"
-        onClick={onClick}
-      >
-        <RiShareFill />
-      </IconButton>
-    </Bounce>
+      <Bounce triggerOnce>
+        <IconButton
+          aria-label="Share selected verses"
+          rounded="full"
+          onClick={onClick}
+        >
+          <RiShareFill />
+        </IconButton>
+      </Bounce>
+    </Box>
   );
 }
